31c2a5d3120cab383d728c42b7aec2db11a4c7db
Mark-Mulligan <mark.mulligan.jr1@gmail.com>
2023-12-07 10:55:56 -0600

    completed part 1 day 5

---
+++ day5/brute_force.py
@@ -0,0 +1,66 @@
+from pprint import pprint
+
+
+class Solution:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.seeds = []
+        self.maps = {}
+        self.read_input()
+
+    def read_input(self):
+        with open(self.file_path) as input:
+            current_map = ""
+
+            for line in input:
+                formatted_line = line.strip()
+
+                if "seeds" in formatted_line:
+                    self.seeds = [
+                        int(seed)
+                        for seed in formatted_line.split(":")[1].strip().split(" ")
+                    ]
+
+                if "map" in formatted_line:
+                    current_map = formatted_line.split(" ")[0].strip()
+                    continue
+
+                if not formatted_line:
+                    current_map = ""
+                    continue
+
+                if current_map:
+                    if current_map not in self.maps:
+                        self.maps[current_map] = {}
+
+                    [end, start, map_range] = [
+                        int(num) for num in formatted_line.split(" ")
+                    ]
+
+                    for i in range(start, start + map_range, 1):
+                        print(i)
+                        count_index = i - start
+                        self.maps[current_map][i] = end + count_index
+
+    def map_seeds(self, map_to_use):
+        new_seed_values = []
+
+        for seed in self.seeds:
+            if seed in self.maps[map_to_use]:
+                new_seed_values.append(self.maps[map_to_use][seed])
+            else:
+                new_seed_values.append(seed)
+
+        self.seeds = new_seed_values
+
+    def get_min_seed_location(self):
+        return min(self.seeds)
+
+
+solution = Solution("./input_day5.txt")
+
+for map in solution.maps:
+    solution.map_seeds(map)
+
+print(solution.seeds)
+print(solution.get_min_seed_location())
